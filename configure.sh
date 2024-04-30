8ce03c2f156cd5f573b505544c7b4023e0c60815
Bluethefox <47904806+bluethefoxofficial@users.noreply.github.com>
2024-04-30 14:00:57 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -13,5 +13,18 @@ sudo apt-get install -y grub-pc-bin
 
 mkdir -p obj
 mkdir -p logs
-mkdir -p Forest
-mkdir -p Forest/boot
+
+mkdir -p iso/boot/grub
+mkdir -p iso/themes/
+mkdir -p iso/themes/default
+mkdir -p iso/themes/default/icons
+mkdir -p iso/themes/default/icons/16x16
+mkdir -p iso/themes/default/icons/32x32
+mkdir -p iso/themes/default/icons/48x48
+mkdir -p iso/themes/default/icons/64x64
+mkdir -p iso/themes/default/sounds/
+
+cp -r Sounds/* iso/themes/default/sounds/
+
+# Copy the grub.cfg file to the iso/boot/grub directory#
+cp Grub/grub.cfg iso/boot/grub/grub.cfg
