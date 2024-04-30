98c2a2ef0660f548c721eab64004f210df7422df
Bluethefox <47904806+bluethefoxofficial@users.noreply.github.com>
2024-04-30 12:32:55 +0100

    added configure file

---
+++ configure.sh
@@ -0,0 +1,9 @@
+
+# Configure by installing the necessary packages
+
+# Install the necessary packages
+sudo apt-get install -y build-essential
+sudo apt-get install -y llvm
+sudo apt-get install -y clang
+sudo apt-get install -y cmake
+sudo apt-get install -y libboost-all-dev
